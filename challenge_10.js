85b4a209eb5e688cd055d19b227d768f844e8f86
jahsan87 <jahsan87@gmail.com>
2014-03-12 12:49:22 -1000

    completed test

--- challenge_10.js
+++ challenge_10.js
@@ -1,1 +1,41 @@
 // do work here
+//export challenge 10
+exports.SummationOfPrimes = SummationOfPrimes;
+
+//Class object to find the sum of primes
+function SummationOfPrimes(limit) {
+
+    //storing parameter into variable to be used in instance methods
+    this.limit = limit;
+
+    //instance method to find the sum of all primes up to the max value limit
+    this.findSumOfPrimes = function () {
+        //flag to set true if number is prime
+        var isPrime = false;
+        //stores the sum of primes
+        var primeSum = 2;
+        var counter = 3;
+
+        //loop through all the numbers until the max value limit
+        while (counter < this.limit) {
+            //if prime, set flag to true, else break out
+            for (var j = 2; j < counter; j++) {
+                if (counter%j !== 0) {
+                    isPrime = true;
+                } else {
+                    isPrime = false;
+                    break;
+                }
+            }
+            //if flag was set to true, add prime to primeSum
+            if (isPrime) {
+                primeSum += counter;
+            }
+            counter += 2; 
+
+        }
+        
+        return primeSum;
+    }
+
+}
--- spec/challenge_10-spec.js
+++ spec/challenge_10-spec.js
@@ -1,5 +1,12 @@
 // import challenge 10
+var SummationOfPrimes = require("../challenge_10").SummationOfPrimes;
 
 // describe the class SummationOfPrimes
+describe("The sum of all primes", function () {
 
 // write your tests here
+    it("below 2,000,000 is", function () {
+        var sum = new SummationOfPrimes(2000000);
+        expect( sum.findSumOfPrimes() ).toEqual(142913828922);
+    });
+});
